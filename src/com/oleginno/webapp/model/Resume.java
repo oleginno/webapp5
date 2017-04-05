d8440d2b359fe8202b6e2f9330bdcc3442e05773
oleginnuendo@gmail.com <oleginnuendo@gmail.com>
2017-04-05 23:54:46 +0300

    test33

---
+++ src/com/oleginno/webapp/model/Resume.java
@@ -0,0 +1,13 @@
+package com.oleginno.webapp.model;
+
+public class Resume {
+    private String fullName;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
