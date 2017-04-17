411e4c2513c001b9b2f02003745507c0c81b2719
oleginnuendo@gmail.com <oleginnuendo@gmail.com>
2017-04-17 20:59:07 +0300

    Add resume constructors

--- src/com/oleginno/webapp/model/Resume.java
+++ src/com/oleginno/webapp/model/Resume.java
@@ -1,6 +1,8 @@
 package com.oleginno.webapp.model;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.UUID;
 
 
 public class Resume {
@@ -13,14 +15,51 @@ public class Resume {
 
     private String homePage;
 
-    private List<Contact> contacts;
+    private List<Contact> contacts = new LinkedList<>();
 
-    private List<Section> sections;
+    private List<Section> sections = new LinkedList<>();
 
 
     public Resume(String fullName, String location) {
+        this(UUID.randomUUID().toString(), fullName, location);
+    }
+
+    public Resume(String uuid, String fullName, String location) {
+        this.uuid = uuid;
         this.fullName = fullName;
         this.location = location;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getHomePage() {
+        return homePage;
+    }
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    public List<Section> getSections() {
+        return sections;
+    }
+
+    public void addSection(Section section) {
+        sections.add(section);
+    }
+
+    public void addContact(Contact contact) {
+        contacts.add(contact);
     }
 
     @Override
