41555f99be07423b83cd9251b266cde4a8339a1a
oleginnuendo@gmail.com <oleginnuendo@gmail.com>
2017-04-17 21:25:57 +0300

    add junit

---
+++ test/com/oleginno/webapp/storage/ArrayStorageTest.java
@@ -0,0 +1,37 @@
+package com.oleginno.webapp.storage;
+
+import org.junit.Test;
+
+/**
+ * Oleh Savych
+ * 17.04.17
+ */
+public class ArrayStorageTest {
+    @Test
+    public void clear() throws Exception {
+    }
+
+    @Test
+    public void save() throws Exception {
+    }
+
+    @Test
+    public void update() throws Exception {
+    }
+
+    @Test
+    public void load() throws Exception {
+    }
+
+    @Test
+    public void delete() throws Exception {
+    }
+
+    @Test
+    public void getAllSorted() throws Exception {
+    }
+
+    @Test
+    public void size() throws Exception {
+    }
+}
